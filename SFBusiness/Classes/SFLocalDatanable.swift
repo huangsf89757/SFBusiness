30e2282a80982153664d923f3f79a88b7cb51e61
hsf89757 <hsf89757@gmail.com>
2024-11-25 13:46:01 +0800

    fix

--- SFBusiness/Classes/SFLocalDatanable.swift
+++ SFBusiness/Classes/SFLocalDatanable.swift
@@ -10,15 +10,15 @@ import Foundation
 public protocol SFLocalDatanable {
     /// 序号
     /// 自增索引
-    var orderL: Int {get set}
+    var orderL: Int? {get set}
     
     /// 唯一标识
     /// MD5
-    var idL: String {get set}
+    var idL: String? {get set}
     
     /// 创建时间
     /// yyyy/MM/dd HH:mm:ss Z
-    var createTimeL: String {get set}
+    var createTimeL: String? {get set}
     
     /// 更新时间
     /// yyyy/MM/dd HH:mm:ss Z
