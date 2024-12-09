1a225034c4c2a1902563279ee08d64521ec11614
hsf89757 <hsf89757@gmail.com>
2024-12-09 21:25:27 +0800

    UserApi

--- SFBusiness/Classes/DataService/SFDataProvider.swift
+++ SFBusiness/Classes/DataService/SFDataProvider.swift
@@ -16,11 +16,49 @@ public typealias SFDataResponse = (code: SFDataResponseCode, isSuccess: Bool, da
 
 // MARK: - SFDataResponseCode
 public enum SFDataResponseCode: Int {
-    case ok = 200
-    case unauthorized = 401
-    case serverError = 500
-    case timeout = 504
+    case ok = 200              // 请求成功
+    case badRequest = 400      // 请求错误
+    case unauthorized = 401    // 未授权
+    case serverError = 500     // 服务器错误
+    case timeout = 504         // 请求超时
+
+    var desc: String {
+        switch self {
+        case .ok:
+            return "Request succeeded."
+        case .badRequest:
+            return "Bad request."
+        case .unauthorized:
+            return "Unauthorized access."
+        case .serverError:
+            return "Server encountered an error."
+        case .timeout:
+            return "Request timed out."
+        }
+    }
+
+    var isSuccessful: Bool {
+        return self == .ok
+    }
+
+    public init?(rawValue: Int) {
+        switch rawValue {
+        case 200:
+            self = .ok
+        case 400:
+            self = .badRequest
+        case 401:
+            self = .unauthorized
+        case 500:
+            self = .serverError
+        case 504:
+            self = .timeout
+        default:
+            return nil 
+        }
+    }
 }
+
 
 // MARK: - SFDataProvider
 public protocol SFDataProvider {
