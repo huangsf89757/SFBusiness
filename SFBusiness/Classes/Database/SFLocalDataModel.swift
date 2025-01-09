aea2e6cbc2f432393bde367f804847d59f344503
hsf89757 <hsf89757@gmail.com>
2025-01-09 09:36:42 +0800

    fix: 数据库默认值优化

--- SFBusiness/Classes/Database/SFLocalDataModel.swift
+++ SFBusiness/Classes/Database/SFLocalDataModel.swift
@@ -13,45 +13,35 @@ import SFBase
 public protocol SFLocalDatanable: SFDatanable {
     /// 序号
     /// 自增索引
-    var orderL: Int? {set get}
+    var orderL: Int {set get}
     
     /// 唯一标识
     /// MD5
-    var idL: String? {set get}
+    var idL: String {set get}
     
     /// 创建时间
     /// yyyy/MM/dd HH:mm:ss Z
-    var createTimeL: String? {set get}
+    var createTimeL: String {set get}
     
     /// 更新时间
     /// yyyy/MM/dd HH:mm:ss Z
-    var updateTimeL: String? {set get}
+    var updateTimeL: String {set get}
 }
 
 extension SFLocalDatanable {
-    public var createDateL: Date? {
+    public var createDateL: Date {
         get {
-            guard let createTimeL = createTimeL else { return nil }
-            return SFDateFormatter.yyyyMMddHHmmssZ.date(from: createTimeL)
+            SFDateFormatter.yyyyMMddHHmmssZ.date(from: createTimeL) ?? Date(timeIntervalSince1970: 0)
         }
         set {
-            guard let newValue = newValue else {
-                createTimeL = nil
-                return
-            }
             createTimeL = SFDateFormatter.yyyyMMddHHmmssZ.string(from: newValue)
         }
     }
-    public var updateDateL: Date? {
+    public var updateDateL: Date {
         get {
-            guard let createTimeL = updateTimeL else { return nil }
-            return SFDateFormatter.yyyyMMddHHmmssZ.date(from: createTimeL)
+            SFDateFormatter.yyyyMMddHHmmssZ.date(from: createTimeL) ?? Date(timeIntervalSince1970: 0)
         }
         set {
-            guard let newValue = newValue else {
-                updateTimeL = nil
-                return
-            }
             updateTimeL = SFDateFormatter.yyyyMMddHHmmssZ.string(from: newValue)
         }
     }
